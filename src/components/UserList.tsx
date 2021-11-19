5e98750efc96225e7367800af22d9204b94b8dc3
DanilovG103 <danilov2706@gmail.com>
2021-11-19 11:27:42 +0400

    fix: pagination

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,25 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { observer } from 'mobx-react-lite'
+import { Table } from 'react-bootstrap'
 import users from '../store/users'
-import { Table} from 'react-bootstrap'
 import { TableRow } from './TableRow'
 
-interface Props {
-  page: number
-}
-
-export const UserList = observer(({ page }: Props) => {
+export const UserList = observer(() => {
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
     users.fetchUsers(page)
   },[page])
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler)
+    return () => {
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  },[])
+
+  const scrollHandler = (event: any) => {
+    if (event.target.documentElement.scrollHeight - (event.target.documentElement.scrollTop + window.innerHeight) < 100) {
+      setPage(prev => prev + 1)
+    }
+  }
 
   if (!!users.error) {
     return <h1>{users.error}</h1>
   }
 
   return (
-    <Table>
+    <Table size='large'>
       <thead>
         <tr>
           <th>Name</th>
@@ -28,7 +38,6 @@ export const UserList = observer(({ page }: Props) => {
           <th>Status</th>
         </tr>
       </thead>
-      <colgroup span={4}></colgroup>
       <tbody>
       {users.users.map(user => (
         <TableRow user={user} key={user.id}/>
